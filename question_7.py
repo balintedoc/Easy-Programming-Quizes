894c0b07743e0e91a12eb032da741947b525e383
balintedoc <orosz.balint.gm@gmail.com>
2026-01-06 19:04:49 +0000

    Add files via upload

---
+++ question_7.py
@@ -0,0 +1,18 @@
+score = 0
+
+print("Welcome to the Programming Quiz(7)!")
+
+#Question 7:
+print("Which Data structure uses key-value pairs?")
+print("A. List")
+print("B. Tuple")
+print("C. Set")
+print("D. Dictionary")
+
+answer =input("Your Answer: ").upper()
+
+if answer == "D" or "D.":
+    print("Correct Answer.")
+    score +=1
+else:
+    print("Wrong Answer. The correct Answer is D.")
